4c3227bd67dddf94cb47fae3f48ca553d1aba017
Magicolo <straty91@hotmail.com>
2016-01-30 18:11:31 -0500

    fix...

--- Assets/Scripts/Systems/TriggerEventOnInputSystem.cs
+++ Assets/Scripts/Systems/TriggerEventOnInputSystem.cs
@@ -11,7 +11,7 @@ public class TriggerEventOnInputSystem : SystemBase, IUpdateable
 	{
 		return EntityManager.Entities.Filter(new[]
 		{
-			typeof(void)
+			typeof(TriggerEventOnInputComponent)
 		});
 	}
 
