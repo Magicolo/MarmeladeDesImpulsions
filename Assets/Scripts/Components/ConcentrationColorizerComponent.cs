95176e1ef4544f5cae07bf89625778d7a6e1d729
Magicolo <straty91@hotmail.com>
2016-01-30 03:28:45 -0500

    Bell Puzzle in tha worx

---
+++ Assets/Scripts/Components/ConcentrationColorizerComponent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Pseudo;
+
+[RequireComponent(typeof(TimeComponent))]
+public class ConcentrationColorizerComponent : ComponentBehaviour
+{
+	public SpriteRenderer Renderer;
+	public MinMax Concentration = new MinMax(0f, 1f);
+	public Gradient Color;
+	public float FadeSpeed = 3f;
+}
---
+++ Assets/Scripts/Components/TriggerConcentrationEventComponent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Pseudo;
+
+public class TriggerConcentrationEventComponent : ComponentBehaviour
+{
+	public float Threshold = 1f;
+	public Events Event;
+}
