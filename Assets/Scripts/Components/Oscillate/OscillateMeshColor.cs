f5ab44e85ae70e0490127d5be9782d6fdb6efbf3
Richard <Rail.rich@gmail.com>
2016-01-31 13:29:19 -0500

    asd

---
+++ Assets/Scripts/Components/Oscillate/OscillateMeshColor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Pseudo;
+
+public class OscillateMeshColor : Oscillator
+{
+	public Material TargetMaterial;
+	public Color Frequency;
+	public Color Amplitude;
+	public Color Center;
+	public Channels Channels;
+
+	public override void Oscillate(float time)
+	{
+		TargetMaterial.color = TargetMaterial.color.Oscillate(Frequency, Amplitude, Center, 0, time, Channels);
+	}
+}
